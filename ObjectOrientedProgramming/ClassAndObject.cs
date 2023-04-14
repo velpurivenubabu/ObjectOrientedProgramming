2385948ee997f42b80eaacd54c5aacd66fa68233
venubabu <velpurivenubabu123@gmail.com>
2023-04-14 23:31:32 +0530

    [Venubabu] Class and Object Created

---
+++ ObjectOrientedProgramming/ClassAndObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgramming
+{
+    internal class ClassAndObject
+    {
+        class Car //Initilizig the class Object
+        {
+            public string color = "red";
+            public string brand = "BMW";
+        }
+        public static void CreateClassAndObject()
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.color);
+            Console.WriteLine(myObj.brand);
+        }
+    }
+
+    
+}
--- ObjectOrientedProgramming/Program.cs
+++ ObjectOrientedProgramming/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace ObjectOrientedProgramming
+﻿using System;
+
+namespace ObjectOrientedProgramming
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Object Oriented Programming");
+            Console.WriteLine("Please Enter any below option");
+            Console.WriteLine("1.ObjectAndClass");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    ClassAndObject.CreateClassAndObject();
+                    break;
+            }
+
         }
     }
 }
