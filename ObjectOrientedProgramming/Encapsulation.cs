d2ced1c2fbd54ac8bf14a368d87e438a2937dbdd
venubabu <velpurivenubabu123@gmail.com>
2023-04-14 23:51:31 +0530

    [Venubabu] Encapsulation Done

---
+++ ObjectOrientedProgramming/Encapsulation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgramming
+{
+    class Human
+    {
+        private string name; // field
+        public string Name   // property
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+    
+  class Encapsulation
+  {
+      public static void EncapsulationMethod()
+      {
+         Human myObj = new Human();
+         myObj.Name = "Venu";
+         Console.WriteLine(myObj.Name);
+      }
+  }
+
+}
+
--- ObjectOrientedProgramming/Program.cs
+++ ObjectOrientedProgramming/Program.cs
@@ -8,13 +8,17 @@ namespace ObjectOrientedProgramming
         {
             Console.WriteLine("Welcome To Object Oriented Programming");
             Console.WriteLine("Please Enter any below option");
-            Console.WriteLine("1.ObjectAndClass");
+            Console.WriteLine("1.ObjectAndClass\n2.Encapsulation");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     ClassAndObject.CreateClassAndObject();
                     break;
+                case 2:
+                    Encapsulation.EncapsulationMethod();
+                    break;
+
             }
 
         }
