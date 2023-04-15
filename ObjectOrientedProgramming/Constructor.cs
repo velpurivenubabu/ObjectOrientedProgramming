a87c95233700cd42b15e55108c3b77e080a7298e
venubabu <velpurivenubabu123@gmail.com>
2023-04-15 06:29:09 +0530

    [Venubabu] done with Constructor

---
+++ ObjectOrientedProgramming/Constructor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgramming
+{
+    class Bus
+    {
+        public string model;
+        public Bus()
+        {
+            model = "Acc5006";
+        }
+    }
+    internal class Constructor
+    {
+        public static void ConstructorMethod()
+        {
+            Bus Mahendra = new Bus();
+            Console.WriteLine(Mahendra.model);
+        }
+    }
+}
